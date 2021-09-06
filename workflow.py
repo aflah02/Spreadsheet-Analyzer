822decbb34e975b2c9844fe4b6ac239bd203ab21
Aflah <aflah20082@iiitd.ac.in>
2021-09-07 01:09:52 +0530

    Describe

--- workflow.py
+++ workflow.py
@@ -4,6 +4,7 @@ path = 'test.csv'
 df = pd.read_csv(path)
 buffer = io.StringIO()
 df.info(buf=buffer)
+df.describe().to_csv("my_description.csv")
 s = buffer.getvalue()
 with open("df_info.txt", "w", encoding="utf-8") as f:
     f.write(s)
